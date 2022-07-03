95951baf6f32e67a9fa1ac3c76730f07d0699994
TonSuchat <suchat.v26@gmail.com>
2022-07-03 20:00:40 +0700

    implement search endpoint

--- src/main/kotlin/com/example/routes/SearchHeroRoute.kt
+++ src/main/kotlin/com/example/routes/SearchHeroRoute.kt
@@ -1,4 +1,21 @@
 package com.example.routes
 
-class SearchHeroRoute {
+import com.example.repository.HeroRepository
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
+
+fun Routing.searchHeroRoute() {
+    val heroRepository: HeroRepository by inject()
+
+    get("/boruto/heroes/search") {
+        val name = call.request.queryParameters["name"]
+        val response = heroRepository.searchHeroes(name)
+        call.respond(
+            message = response,
+            status = HttpStatusCode.OK
+        )
+    }
 }
