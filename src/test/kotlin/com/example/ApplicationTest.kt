18595b7e49203646d3a4222f5b5f6ea649252e36
TonSuchat <suchat.v26@gmail.com>
2022-07-07 16:35:34 +0700

    add test for heroes endpoint

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,29 +1,50 @@
 package com.example
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.plugins.callloging.*
-import org.slf4j.event.*
-import io.ktor.server.request.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
+import com.example.constants.Hero1
+import com.example.models.ApiResponse
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlin.test.*
+import io.ktor.http.*
 import io.ktor.server.testing.*
-import com.example.plugins.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
-    fun testRoot() = testApplication {
-        application {
-            configureRouting()
-        }
+    fun `access route endpoint, assert correct`() = testApplication {
         client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
+            assertEquals(
+                expected = HttpStatusCode.OK,
+                actual = status
+            )
+            assertEquals(
+                expected = "Welcome to Boruto API!",
+                actual = bodyAsText().trim('"')
+            )
+        }
+    }
+
+    @Test
+    fun `access all heroes endpoint, assert correct`() = testApplication {
+        client.get("/boruto/heroes").apply {
+            val expectedApiResponse = ApiResponse(
+                success = true,
+                message = "ok",
+                prevPage = null,
+                nextPage = 2,
+                heroes = Hero1
+            )
+            assertEquals(
+                expected = HttpStatusCode.OK,
+                actual = status
+            )
+            val actual = Json.decodeFromString<ApiResponse>(bodyAsText())
+            assertEquals(
+                expected = expectedApiResponse,
+                actual = actual
+            )
         }
     }
 }
